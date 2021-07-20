84e82ff86e5f686f221230aa970fd4fb6ba1f45c
evopen <520dhh@gmail.com>
2021-07-20 21:56:40 +0800

    add some rand function

--- shaders/ray-tracing/src/util.rs
+++ shaders/ray-tracing/src/util.rs
@@ -16,3 +16,32 @@ pub fn facefoward(n: &Vec3, i: &Vec3) -> Vec3 {
         false => -*n,
     }
 }
+
+#[inline]
+pub fn rand_vec3_range(state: &mut u32, min: f32, max: f32) -> Vec3 {
+    let scale = max - min;
+    let n1 = rng(state) * scale + min;
+    let n2 = rng(state) * scale + min;
+    let n3 = rng(state) * scale + min;
+    Vec3::new(n1, n2, n3)
+}
+
+pub fn rand_vec3_in_unit_sphere(state: &mut u32) -> Vec3 {
+    loop {
+        let p = rand_vec3_range(state, -1.0, 1.0);
+        if p.length_squared() >= 1.0 {
+            continue;
+        } else {
+            return p;
+        }
+    }
+}
+
+pub fn rand_vec3_unit(state: &mut u32) -> Vec3 {
+    rand_vec3_in_unit_sphere(state).normalize()
+}
+
+pub fn is_near_zero(v: Vec3) -> bool {
+    let epsilon = 1e-8;
+    v.abs().cmple(Vec3::splat(epsilon)).all()
+}
