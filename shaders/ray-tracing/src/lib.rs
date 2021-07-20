a3c0719577cc1c80c08c966e56c21c8744bae200
evopen <520dhh@gmail.com>
2021-07-20 22:00:04 +0800

    update, recursion part don't work

--- shaders/ray-tracing/src/lib.rs
+++ shaders/ray-tracing/src/lib.rs
@@ -28,12 +28,18 @@ pub struct CameraInfo {
     projection_inv: Mat4,
 }
 
+pub struct Payload {
+    color: Vec3,
+    depth: u32,
+    rng_state: u32,
+}
+
 #[spirv(ray_generation)]
 pub fn main(
     #[spirv(push_constant)] camera_info: &CameraInfo,
     #[spirv(launch_id)] pixel: UVec3,
     #[spirv(launch_size)] launch_size: UVec3,
-    #[spirv(ray_payload)] payload: &mut Vec3,
+    #[spirv(ray_payload)] payload: &mut Payload,
     #[spirv(descriptor_set = 0, binding = 0)] tlas: &spirv_std::ray_tracing::AccelerationStructure,
     // #[spirv(descriptor_set = 0, binding = 1)] img: &Image!(2D, type=f32, sampled=false),
     #[spirv(descriptor_set = 1, binding = 0)] color_image: &mut image::Image<
@@ -74,6 +80,9 @@ pub fn main(
         let target = camera_info.projection_inv * d.extend(1.0).extend(1.0);
         let target_norm = (target.xyz() / target.w).normalize();
         let direction = (camera_info.view_inv * target_norm.extend(0.0)).normalize();
+
+        payload.depth = 10;
+        payload.rng_state = 10086;
         tlas.trace_ray(
             spirv_std::ray_tracing::RayFlags::OPAQUE,
             0xFF,
@@ -89,7 +98,7 @@ pub fn main(
 
         let xy = UVec2::new(pixel.x, launch_size.y - pixel.y);
 
-        color_image.write(xy, payload.extend(1.0));
+        color_image.write(xy, payload.color.extend(1.0));
     }
 }
 
@@ -125,7 +134,7 @@ pub struct MaterialInfo {
 
 #[spirv(closest_hit)]
 pub fn closest_hit(
-    #[spirv(incoming_ray_payload)] payload: &mut Vec3,
+    #[spirv(incoming_ray_payload)] payload: &mut Payload,
     #[spirv(hit_attribute)] hit_attr: &mut Vec2,
     #[spirv(instance_id)] instance_id: usize, // index of instance in tlas
     #[spirv(ray_geometry_index)] geometry_index: usize, // index of geometry in instance
@@ -138,6 +147,7 @@ pub fn closest_hit(
     #[spirv(object_ray_direction)] object_ray_direction: Vec3,
     #[spirv(shader_record_buffer)] shader_record_buffer: &mut ShaderRecordData,
     // #[spirv(world_to_object)] world_to_object: glam::Affine3A,
+    #[spirv(descriptor_set = 0, binding = 0)] tlas: &spirv_std::ray_tracing::AccelerationStructure,
     #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] index_buffer: &mut [u32],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 2)] vertex_buffer: &mut [f32],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 3)] geometry_infos: &mut [GeometryInfo], // per-BLAS
@@ -150,6 +160,14 @@ pub fn closest_hit(
     #[spirv(storage_buffer, descriptor_set = 0, binding = 10)] tex_coord_buffer: &mut [Vec2],
     #[spirv(push_constant)] camera_info: &CameraInfo,
 ) {
+    payload.depth -= 1;
+    if payload.depth == 0 {
+        payload.color = Vec3::splat(0.0);
+        return;
+    }
+    let tmin = 0.001;
+    let tmax = 10000.0;
+
     let barycentrics = vec3(1.0 - hit_attr.x - hit_attr.y, hit_attr.x, hit_attr.y);
 
     let geometry_info =
@@ -203,17 +221,35 @@ pub fn closest_hit(
                 unsafe { samplers.index(material_info.base_color_sampler_index as usize) };
             let image = unsafe { images.index(material_info.base_color_image_index as usize) };
             let texel: Vec4 = image.sample_by_lod(*sampler, tex_coord, 0.0);
-            let color = texel * material_info.base_color_factor;
-            *payload = color.xyz();
+            unsafe {
+                let mut scatter_direction =
+                    world_normal + util::rand_vec3_unit(&mut payload.rng_state);
+                if util::is_near_zero(scatter_direction) {
+                    scatter_direction = world_normal;
+                }
+                tlas.trace_ray(
+                    spirv_std::ray_tracing::RayFlags::OPAQUE,
+                    0xFF,
+                    0,
+                    0,
+                    0,
+                    world_position,
+                    tmin,
+                    scatter_direction,
+                    tmax,
+                    payload,
+                );
+            }
+            payload.color = (texel * material_info.base_color_factor * 0.5).xyz() * payload.color;
         } else if geometry_info.has_color == 1 {
             let v0_color = color_buffer[color_offset + v0_index];
             let v1_color = color_buffer[color_offset + v1_index];
             let v2_color = color_buffer[color_offset + v2_index];
             let color =
                 v0_color * barycentrics.x + v1_color * barycentrics.y + v2_color * barycentrics.z;
-            *payload = (color * material_info.base_color_factor).xyz();
+            payload.color = (color * material_info.base_color_factor).xyz();
         } else {
-            *payload = material_info.base_color_factor.xyz();
+            payload.color = material_info.base_color_factor.xyz();
         }
     }
 
@@ -248,7 +284,7 @@ pub fn closest_hit(
 
 #[spirv(miss)]
 pub fn miss(
-    #[spirv(incoming_ray_payload)] payload: &mut Vec3,
+    #[spirv(incoming_ray_payload)] payload: &mut Payload,
     #[spirv(world_ray_direction)] world_ray_direction: Vec3,
     #[spirv(descriptor_set = 1, binding = 2)] sampler: &Sampler,
     #[spirv(descriptor_set = 2, binding = 0)] sky_texture: &image::Image<
@@ -265,12 +301,12 @@ pub fn miss(
     // *payload = vec3(1.0, 0.5, 0.23);
     let coord = sample_sphereical_map(&world_ray_direction);
     let color: Vec4 = sky_texture.sample_by_lod(*sampler, coord, 0.0);
-    *payload = color.xyz();
+    payload.color = color.xyz() * 2.0;
 }
 
 pub fn sample_sphereical_map(direction: &Vec3) -> Vec2 {
     let inv_atan = vec2(0.1591, 0.3183);
-    let mut uv = vec2(direction.z.atan2(direction.x), direction.y.asin());
+    let mut uv = vec2(direction.z.atan2(direction.x), -direction.y.asin());
     uv *= inv_atan;
     uv += Vec2::splat(0.5);
     return uv;
